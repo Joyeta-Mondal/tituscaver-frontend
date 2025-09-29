b763960b82c34d9d0387a8f5e65cd6e5479df1ab
Sourav-Dn-p <sdsouravdebnath26@gmail.com>
2025-09-29 14:12:04 +0600

    service page done

--- src/app/(LandingPage)/servicesLanding/page.tsx
+++ src/app/(LandingPage)/servicesLanding/page.tsx
@@ -1,7 +1,17 @@
+import Expart from "@/components/features/aboutUs/expart/Expart";
+import Transactions from "@/components/features/landingPage/transactions/Transactions";
+import WhatWeDo from "@/components/features/landingPage/whatWeDo/WhatWeDo";
+import ServicesBanner from "@/components/features/Services/banner/ServicesBanner";
+
 export default function ServiceLandingPage() {
   return (
     <div>
-      <h1>ServiceLandingPage</h1>
+      <main>
+        <ServicesBanner />
+        <WhatWeDo />
+        <Transactions />
+        <Expart />
+      </main>
     </div>
   );
 }
---
+++ src/components/features/Services/banner/ServicesBanner.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Image from "next/image";
+
+export default function ServicesBanner() {
+  return (
+    <section className="relative flex h-screen w-full justify-center text-center md:h-[400px]">
+      {/* Image background */}
+      <Image
+        src="/images/landingPage/ServicePage/serviceBanner.png"
+        alt="about banner"
+        width={1600}
+        height={1600}
+        quality={100}
+        className="absolute inset-0 h-full w-full object-cover"
+      />
+
+      {/* Overlay for better text readability */}
+      <div className="absolute inset-0 bg-gradient-to-r from-black/65 via-black/80 to-black/65"></div>
+
+      {/* Banner content */}
+      <div className="align-item-center relative z-10 mx-auto flex h-full max-w-[1440px] flex-col items-center justify-center px-8 text-center text-white md:items-start md:text-left lg:px-0">
+        <h1 className="text-3xl font-bold md:text-5xl lg:text-7xl">About Us</h1>
+
+        <p className="text-md mx-auto mt-4 max-w-2xl text-center text-gray-200 md:max-w-3xl md:text-lg lg:max-w-4xl lg:text-2xl">
+          Home / About Us
+        </p>
+      </div>
+    </section>
+  );
+}
